bc683e1748977ca7cb8f16bd777ac8e8dfdaae76
Safder <safder.iqbal@gmail.com>
2012-11-14 15:11:46 +0000

    First commit from the video tutorials.

---
+++ Hello_World/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hello_World
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadLine();
+		}
+	}
+}

cecbd1c8d445d758220583e40467d34d14456971
Safder <safder.iqbal@gmail.com>
2012-11-21 15:56:39 +0000

    Covered conditional operatators, operands, and the for statement. More a review than learning here.

---
+++ ForIterations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForIterations
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				//Console.WriteLine(i.ToString());
+				if (i == 7)
+				{
+					Console.WriteLine("Found seven!");
+					break;
+				}
+			}
+
+			Console.ReadKey();
+		}
+	}
+}

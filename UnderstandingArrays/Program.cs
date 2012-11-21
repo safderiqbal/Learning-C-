4f6d7c7c6f0043d4cdd2fb8fd9591f08ca388255
Safder <safder.iqbal@gmail.com>
2012-11-21 16:21:14 +0000

    Modified the gitignore to leave out the 'AssemblyInfo.cs' files and committed the first work on understanding arrays. Again, it's more like a refresher than actual learning at the moment.

---
+++ UnderstandingArrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnderstandingArrays
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//int[] numbers = new int[5];
+			//numbers[0] = 4;
+			//numbers[1] = 8;
+			//numbers[2] = 15;
+			//numbers[3] = 16;
+			//numbers[4] = 23;
+			//numbers[5] = 42;
+
+
+			int[] numbers = { 4, 8, 15, 16, 23 };
+
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				Console.WriteLine(numbers[i]);
+			}
+
+			//Console.WriteLine(numbers[1]);
+			Console.ReadKey();
+		}
+	}
+}

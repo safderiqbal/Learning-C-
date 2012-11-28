3b2f398cdf77d094be2c2afea1ca60673800353b
Safder Iqbal <safder.iqbal@gmail.com>
2012-11-28 17:15:55 +0000

    Finished up 'while' lesson

---
+++ ReadTextFileWhile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReadTextFileWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader myReader = new StreamReader("Values.txt");
+
+            string line = "";
+
+            while (line != null)
+            {
+                line = myReader.ReadLine();
+                if (line != null)
+                    Console.WriteLine(line);
+            }
+
+            myReader.Close();
+            Console.ReadLine();
+        }
+    }
+}

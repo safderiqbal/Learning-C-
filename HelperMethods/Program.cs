26c7cc0ce5b964df4be3d5acc6ec14dda6676635
Safder Iqbal <safder.iqbal@gmail.com>
2012-11-28 16:36:48 +0000

    Finished the methods lesson

---
+++ HelperMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(superSecretFormula("Bob"));
+
+            Console.WriteLine(superSecretFormula());
+
+            Console.ReadLine();
+        }
+
+        private static string superSecretFormula()
+        {
+            return "Hello World";
+        }
+
+
+        private static string superSecretFormula(string name)
+        {
+            return string.Format("Hello World, {0}",name);
+        }
+    }
+}

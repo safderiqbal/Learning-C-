801cd69724f80c887ddcecce98dded8f484da11e
Safder Iqbal <safder.iqbal@gmail.com>
2012-12-05 15:52:37 +0000

    String manipulation lesson

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -17,7 +17,21 @@ namespace Strings
             //string myString = string.Format("{0}!", "Hello");
             //string myString = string.Format("Make: {0} (Model: {1})", "BMW", "745li");
 
-            string myString = string.Format("{0:C}", 123.45);
+            //string myString = string.Format("{0:C}", 123.45);
+
+            //string myString = string.Format("{0:N}",1234567890);
+
+            //string myString = string.Format("{0:P}", 0.123);
+
+            //string myString = string.Format("Phone number: {0:(###) ###-####}", 1234567890);
+
+            string myString = "";
+
+            for (int i = 0; i < 100; i++)
+            {
+               // myString = myString + "--" + i.ToString();
+                myString += "--" + i.ToString();
+            }
 
             Console.WriteLine(myString);
             Console.ReadLine();

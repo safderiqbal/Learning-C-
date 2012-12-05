a20e7e0e4ffbf575feaea44eb9ea498bb78c2bde
Safder Iqbal <safder.iqbal@gmail.com>
2012-12-05 15:08:36 +0000

    String manipulation lesson

---
+++ Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  String myString = "Go to your C:\\ drive";
+            //String myString = "My \"so called \" life";
+           // string myString = "What if I need\n a new line?";
+
+            //string myString = string.Format("{0}!", "Hello");
+            //string myString = string.Format("Make: {0} (Model: {1})", "BMW", "745li");
+
+            string myString = string.Format("{0:C}", 123.45);
+
+            Console.WriteLine(myString);
+            Console.ReadLine();
+        }
+    }
+}

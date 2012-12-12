ccc4a3a279b149dcd2f277fe77496009b017c008
Safder Iqbal <safder.iqbal@gmail.com>
2012-12-12 15:27:52 +0000

    Started DateTime Lesson.

---
+++ DatesAndTimes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue.ToString());
+            //Console.WriteLine(myValue.ToShortDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+
+            Console.WriteLine(myValue.AddDays(3).ToShortDateString());
+
+            Console.ReadLine();
+        }
+    }
+}

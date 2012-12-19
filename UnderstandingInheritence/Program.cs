e051e542bbaf58531f4eb026ef1e922c60c7c5b6
Safder Iqbal <safder.iqbal@gmail.com>
2012-12-19 15:36:47 +0000

    Initial commit for inheritence lessson

---
+++ UnderstandingInheritence/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingInheritence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "BMW";
+            myCar.Model = "745li";
+            myCar.Colour = "Black";
+            myCar.Year = 2005;
+
+            someMethod(myCar);
+
+            //myCar.PrintMe();
+
+            Console.ReadLine();
+        }
+
+        private static void someMethod(Car car)
+        {
+            Console.WriteLine("From someMethod: {0}", car.Make);
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Colour { get; set; }
+
+        public void PrintMe()
+        {
+            Console.WriteLine("{0} - {1}", this.Make, this.Model);
+        }
+    }
+}
